e133b3fcebdf4a97b72b61a0748135223bf15079
floathandthing <springtrap.bite87@gmail.com>
2018-02-26 12:16:59 +0000

    Blank template for project upload

--- TemperatureMonitor/Form1.Designer.cs
+++ TemperatureMonitor/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TemperatureMonitor
 {
-    partial class Form1
+    partial class Window
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Name = "Window";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
